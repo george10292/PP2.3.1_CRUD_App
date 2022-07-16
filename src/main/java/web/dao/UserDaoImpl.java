95bad9d5c53a8c040c30b97d3f5c5dfccff7a884
Yegor Yaroslavtsev <yaroslavtsevegor@gmail.com>
2022-07-17 02:12:10 +0500

    Initial commit

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,42 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+
+@Repository
+public class UserDaoImpl implements UserDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> allUsers() {
+        return entityManager.createQuery("from User").getResultList();
+    }
+
+    @Override
+    public void add(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void delete(int id) {
+        User delUser = getById(id);
+        entityManager.remove(delUser);
+    }
+
+    @Override
+    public void edit(User user, int id) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User getById(int id) {
+        return entityManager.find(User.class, id);
+    }
+}
